67865dc3ed060c0bfc8d9d04b61d8bceeb1d90ea
gustavocadev <churaalaygustavo11@gmail.com>
2023-02-22 19:36:17 -0500

    feat: add new fn

--- main.ts
+++ main.ts
@@ -2,3 +2,4 @@ console.log('Hola XD')
 export const add = (a: number, b: number) => a + b
 export const sub = (a: number, b: number) => a - b
 export const mul = (a: number, b: number) => a * b
+export const div = (a: number, b: number) => a / b
